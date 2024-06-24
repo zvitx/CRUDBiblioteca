5f10c5ed10d3c960ef7528f63df9c763e86c8ded
Vitor Abreu <152200271+zvitx@users.noreply.github.com>
2024-06-24 20:43:39 -0300

    Add files via upload

---
+++ sql/insert_data.sql
@@ -0,0 +1,8 @@
+USE biblioteca_db;
+
+INSERT INTO Livro (titulo, autor, anopublic, genero, status) VALUES
+('O Senhor dos Anéis', 'J.R.R. Tolkien', '1954-07-29', 'Fantasia', 'disponível'),
+('1984', 'George Orwell', '1949-06-08', 'Distopia', 'disponível'),
+('A Revolução dos Bichos', 'George Orwell', '1945-08-17', 'Distopia', 'disponível'),
+('Harry Potter e a Pedra Filosofal', 'J.K. Rowling', '1997-06-26', 'Fantasia', 'disponível'),
+('O Hobbit', 'J.R.R. Tolkien', '1937-09-21', 'Fantasia', 'disponível');
